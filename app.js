8299a9627670e97977961815fda9842eb94b0a1f
AKC068 <ayushkumac040@gmail.com>
2022-07-03 15:27:27 +0530

    commit

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
   res.render("index");
 });
 
-app.get("/login",function(req,res){
+app.get("/login", function (req, res) {
   res.render("login");
 });
 
-app.get("/signup",function(req,res){
+app.get("/signup", function (req, res) {
   res.render("signup");
 });
 
@@ -28,6 +38,11 @@ app.get("/signup",function(req,res){
 //   res.sendFile(__dirname+"/index.html");
 // });
 
-app.listen(3000,function(){
-  console.log("server started at port 3000");
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDb disconnected!");
+});
+
+app.listen(3000, function () {
+  connect();
+  console.log("Server started at port 3000");
 });
