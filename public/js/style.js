9221b1818faa4721cfbe909e87ce2060e37393e8
AKC068 <ayushkumac040@gmail.com>
2022-07-06 13:31:55 +0530

    navbar-monitor

---
+++ public/js/style.js
@@ -0,0 +1,18 @@
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+// function openNav() {
+// 	document.getElementById("myNav").style.width = "100%";
+// }
+// function closeNav() {
+// 	document.getElementById("myNav").style.width = "0%";
+// }
