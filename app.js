a2811565fca944da1a3c6baea59bbdfbec0bad95
AKC068 <ayushkumac040@gmail.com>
2022-07-14 20:53:11 +0530

    setup

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use("/", authRouter);
 
 // ==============================================
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log("Server started at port " + PORT);
 });
